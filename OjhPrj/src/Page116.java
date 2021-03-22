9eeba465af0b55a65cb095712b24c8fc47b73076
fivejinhwa <fivejinhwa@gmail.com>
2021-03-22 15:19:14 +0900

    page

---
+++ OjhPrj/src/Page116.java
@@ -0,0 +1,20 @@
+
+public class Page116 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a,b,c,d,e;
+		a='A'; //65로 저장
+		System.out.println(" a:  "+a+" |a의 숫자값 "+(int)a);
+		b='a'; //97
+		c= (char) (b+1); //98에 해당되는 문자열을 넣어라 
+		System.out.println(" b: "+b+" |b의 숫자값 "+(int)b);
+		System.out.println(" c: "+c+" |c의 숫자값 "+(int)c);
+	
+		d='가'; //44032
+		e=(char)(d+1); //44033
+		System.out.println(" d: "+d+" |d의 숫자값 "+(int)d);
+		System.out.println(" e: "+e+" |e의 숫자값 "+(int)e);
+	
+	}
+}
