30a14f9a4f6cfcd26dcaa6d023f61351e24e12b7
data33 <fivejinhwa@naver.com>
2021-03-24 11:08:29 +0900

    ㅇㅇㅇ

---
+++ OjhPrj/src/Page168_9.java
@@ -0,0 +1,14 @@
+
+public class Page168_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		
+		if(a<100) {//조건을 추가하겠다.
+			System.out.println("100보다 작군요");
+			System.out.println("100보다 작군요ㅇ"); // 대괄호를 넣어서 범위를 정해줘야 여기까지 조건을 실행해주기 때문에
+		}
+		System.out.println("난 무조건 실행된다"); // 맨밑에만 실행이 됨
+	}
+}
